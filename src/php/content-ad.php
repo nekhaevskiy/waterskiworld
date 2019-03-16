df69a3e37c0921dfcbfcdc9405949e9c952ad470
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2019-03-16 14:11:28 +0300

    Change styles of Ads single page to Tachyons

---
+++ src/php/content-ad.php
@@ -0,0 +1,14 @@
+<article>
+  <h1 class="f2 mv0 lh-solid fw5 serif">
+    <?php the_title(); ?>
+  </h1>
+  <div class="mt3 cf nested-copy-separator nested-img">
+    <?php the_content(); ?>
+  </div>
+  <p class="f5 tr mt3 silver i">
+    Опубликовано:
+    <span>
+      <?php the_date(); ?>
+    </span>
+  </p>
+</article>
--- src/php/single.php
+++ src/php/single.php
@@ -1,24 +1,39 @@
 <?php get_header(); ?>
-  <div class="row">
-    <main class="col-md-10 col-md-offset-1 main main_no-sidebar">
-      <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-          <?php if ( in_category( 9 ) ) { ?>
-            <?php get_template_part( 'content-waterskier', get_post_format() ); ?>
-          <?php } else { ?>
-            <?php get_template_part( 'content-single', get_post_format() ); ?>
-          <?php } ?>
-        <?php endwhile; ?>
-        
+  <main class="center measure-wide f4 mb5 bg-white black-70 lh-copy sans-serif">
+    <?php if ( have_posts() ) : ?>
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php
+          if ( in_category( 'adverts' ) ) {
+            get_template_part( 'content-ad', get_post_format() );
+          } elseif (in_category( 'vodnolyzhniki' )) {
+            get_template_part( 'content-waterskier', get_post_format() );
+          } else {
+            get_template_part( 'content-single', get_post_format() );
+          } 
+        ?>
+      <?php endwhile; ?>
+      
+      <div class="mt3">
         <?php get_template_part( 'partials/facebook-like-button' ); ?>
+      </div>
 
-        <nav>
-          <ul class="pager">
-            <li class="previous"><?php next_post_link( '%link', '<span aria-hidden="true">&larr;</span> След.', true ); ?></li>
-            <li class="next"><?php previous_post_link( '%link', 'Пред. <span aria-hidden="true">&rarr;</span>', true ); ?></li>
-          </ul>
-        </nav>
-      <?php endif; ?>
-    </main>
-  </div> <!-- .row -->
+      <nav class="mt4 tc">
+        <?php
+          $next_post = get_adjacent_post(true, '', false);
+          if (!empty($next_post)) {
+            echo '<a class="f5 mr2 ph3 pv2 dark-blue link dim fw5 dib br1 ba bw1" href="' . get_permalink($next_post->ID) .'"><span aria-hidden="true">&larr;</span> След.</a>';
+          } else {
+            echo '<a class="f5 mr2 ph3 pv2 silver hover-silver link fw5 dib br1 ba bw1"><span aria-hidden="true">&larr;</span> След.</a>';
+          }
+          
+          $prev_post = get_adjacent_post(true, '', true);
+          if (!empty($prev_post)) {
+            echo '<a class="f5 ml2 ph3 pv2 dark-blue link dim fw5 dib br1 ba bw1" href="' . get_permalink($prev_post->ID) .'">Пред. <span aria-hidden="true">&rarr;</span></a>';
+          } else {
+            echo '<a class="f5 ml2 ph3 pv2 silver hover-silver link fw5 dib br1 ba bw1">Пред. <span aria-hidden="true">&rarr;</span></a>';
+          }
+        ?>
+      </nav>
+    <?php endif; ?>
+  </main>
 <?php get_footer(); ?>
