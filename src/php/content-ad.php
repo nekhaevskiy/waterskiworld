044962e03ec1cc0d3ac47b13a5a3994d4efc9f3b
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2019-03-19 07:46:53 +0300

    Combine nested rules in nested class

--- src/php/content-ad.php
+++ src/php/content-ad.php
@@ -2,7 +2,7 @@
   <h1 class="f2 mv0 lh-solid fw5 serif">
     <?php the_title(); ?>
   </h1>
-  <div class="mt3 cf nested-copy-separator nested-img">
+  <div class="mt3 cf nested">
     <?php the_content(); ?>
   </div>
   <p class="f5 tr mt2 silver i">
