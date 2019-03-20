2572a852fc684b82f3e9516d532ccace86cf518f
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2019-03-20 07:39:54 +0300

    Change text for prev/next link

--- src/php/single.php
+++ src/php/single.php
@@ -25,7 +25,7 @@
               <title>chevronLeft icon</title>
               <path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
             </svg>
-            <span class="v-mid pl1">След.</span>
+            <span class="v-mid pr1">Пред.</span>
           ';
           if (!empty($next_post)) {
             echo '<a class="f5 mr2 ph3 pv2 dark-blue link dim fw5 dib br2 ba bw1" href="' . get_permalink($next_post->ID) .'" data-qa="linkNextPage">' . $next_post_text . '</a>';
@@ -35,7 +35,7 @@
           
           $prev_post = get_adjacent_post(true, '', true);
           $prev_post_text = '
-            <span class="v-mid pr1">Пред.</span>
+            <span class="v-mid pl1">След.</span>
             <svg class="v-mid w1" data-icon="chevronRight" viewBox="0 0 32 32" style="fill:currentcolor">
               <title>chevronRight icon</title>
               <path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
