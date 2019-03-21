424e2900264ed655f9be0106d6adefbfcc68d52e
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2019-03-21 08:46:11 +0300

    Fix prev/next link in post for IE

--- src/php/single.php
+++ src/php/single.php
@@ -21,7 +21,7 @@
         <?php
           $next_post = get_adjacent_post(true, '', false);
           $next_post_text = '
-            <svg class="v-mid w1" data-icon="chevronLeft" viewBox="0 0 32 32" style="fill:currentcolor">
+            <svg class="v-mid w1" data-icon="chevronLeft" viewBox="0 0 32 32" style="fill:currentcolor; height: 1rem">
               <title>chevronLeft icon</title>
               <path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
             </svg>
@@ -36,7 +36,7 @@
           $prev_post = get_adjacent_post(true, '', true);
           $prev_post_text = '
             <span class="v-mid pl1">След.</span>
-            <svg class="v-mid w1" data-icon="chevronRight" viewBox="0 0 32 32" style="fill:currentcolor">
+            <svg class="v-mid w1" data-icon="chevronRight" viewBox="0 0 32 32" style="fill:currentcolor; height: 1rem">
               <title>chevronRight icon</title>
               <path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
             </svg>
