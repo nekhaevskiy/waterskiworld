bc5cf2afa74af065696eba5460ca2856c3408af5
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2019-05-02 14:50:37 +0300

    Fix padding between navbar and heading of 404 page

--- src/php/404.php
+++ src/php/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<main class="bg-white black-70 center f4 lh-copy mb5 measure-wide sans-serif">
-    <h1 class="b--light-gray bb f1 fw5 lh-solid mb4 mt2 pb4 serif" data-qa="pageHeading">
+<main class="bg-white black-70 center f4 lh-copy measure-wide pb5 pt4 sans-serif">
+    <h1 class="b--light-gray bb f1 fw5 lh-solid mb4 mt0 pb4 serif" data-qa="pageHeading">
         Упс! Что-то пошло не так...
     </h1>
 
