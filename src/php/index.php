b206dd8171c1bf2588970aed6502eebd62f4471a
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2019-05-03 16:23:22 +0300

    Remove home.php

--- src/php/index.php
+++ src/php/index.php
@@ -1,20 +1,30 @@
 <?php get_header(); ?>
-  <div class="row">
-    <main class="col-md-8 main">
-      <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-          <?php get_template_part( 'content', get_post_format() ); ?>
-        <?php endwhile; ?>
-        <nav>
-          <ul class="pager">
-            <li class="next"><?php next_posts_link( 'Пред. <span aria-hidden="true">&rarr;</span>' ); ?></li>
-            <li class="previous"><?php previous_posts_link( '<span aria-hidden="true">&larr;</span> След.' ); ?></li>
-          </ul>
-        </nav>
-      <?php else :?>
-        <?php echo 'Sorry, no posts matched your criteria.' ?>
-      <?php endif; ?>
+
+<div class="bg-white black-70 center flex-l flex-nowrap justify-between mw8 ph3">
+    <main class="f4 measure-wide pb5">
+        <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
+        <?php $query = new WP_Query(array('cat' => -354, 'paged' => $paged)); ?>
+        <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+                <?php get_template_part('content', get_post_format()); ?>
+            <?php endwhile; ?>
+
+            <nav class="pagination mt5">
+                <ul class="pagination__list">
+                    <li class="pagination__item pagination__item--prev" data-qa="paginationPrevLinkWrapper">
+                        <?php previous_posts_link('Пред.'); ?>
+                    </li>
+                    <li class="pagination__item pagination__item--next" data-qa="paginationNextLinkWrapper">
+                        <?php next_posts_link('След.'); ?>
+                    </li>
+                </ul>
+            </nav>
+
+        <?php else : ?>
+            <?php echo 'К сожалению, мы не смогли найти подходящих постов' ?>
+        <?php endif; ?>
     </main>
+
     <?php get_sidebar(); ?>
-  </div> <!-- .row -->
+</div>
+
 <?php get_footer(); ?>
