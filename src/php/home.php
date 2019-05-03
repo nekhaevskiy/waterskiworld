382113ab4f8c35de37ac2a28639cdf3a9f73b4c4
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2019-05-03 12:16:09 +0300

    Add Tachyons to the homepage

--- src/php/home.php
+++ src/php/home.php
@@ -1,28 +1,30 @@
 <?php get_header(); ?>
 
-<div class="row">
-    <main class="col-md-8 main pb5">
+<div class="bg-white black-70 center flex-l flex-nowrap justify-between mw8 ph3">
+    <main class="f4 measure-wide pb5">
+        <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
+        <?php $query = new WP_Query(array('cat' => -354, 'paged' => $paged)); ?>
+        <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+                <?php get_template_part('content', get_post_format()); ?>
+            <?php endwhile; ?>
 
-        <?php $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1; ?>
-        <?php $query = new WP_Query( array( 'cat' => -354, 'paged' => $paged ) ); ?>   
-        <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-        <?php get_template_part( 'content', get_post_format() ); ?>
-        <?php endwhile; ?>
+            <nav class="wsw-pagination mt5">
+                <ul class="wsw-pagination__list">
+                    <li class="wsw-pagination__item wsw-pagination__item--prev" data-qa="paginationPrevLinkWrapper">
+                        <?php previous_posts_link('Пред.'); ?>
+                    </li>
+                    <li class="wsw-pagination__item wsw-pagination__item--next" data-qa="paginationNextLinkWrapper">
+                        <?php next_posts_link('След.'); ?>
+                    </li>
+                </ul>
+            </nav>
 
-        <nav>
-          <ul class="pager">
-            <li class="next"><?php next_posts_link( 'Пред. <span aria-hidden="true">&rarr;</span>' ); ?></li>
-            <li class="previous"><?php previous_posts_link( '<span aria-hidden="true">&larr;</span> След.' ); ?></li>
-          </ul>
-        </nav>
-
-        <?php else :?>
-        <?php echo 'Sorry, no posts matched your criteria.' ?>
+        <?php else : ?>
+            <?php echo 'К сожалению, мы не смогли найти подходящих постов' ?>
         <?php endif; ?>
-
     </main>
 
     <?php get_sidebar(); ?>
-</div> <!-- .row -->
+</div>
 
 <?php get_footer(); ?>
